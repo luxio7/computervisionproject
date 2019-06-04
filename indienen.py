8434a2bc60452a22979c6af4828a270c284cb262
Unknown <rik.p@live.be>
2019-06-04 22:47:32 +0200

    'indienen' is de file met mijn laatste versie

--- indienen.py
+++ indienen.py
@@ -7,12 +7,11 @@ from keras.utils import to_categorical
 from keras import backend as K
 import numpy as np
 from keras import optimizers
-from keras.models import Sequential 
 import pickle
 from matplotlib import pyplot as plt
 from keras.utils.vis_utils import plot_model
-import sys
 import random
+import sys
 import datetime
 from lxml import etree
 
@@ -20,8 +19,8 @@ from lxml import etree
 filter = ['aeroplane', 'car', 'chair', 'dog', 'bird']       # select class, this default should yield 1489 training and 1470 validation images
 voc_root_folder = "D:/computervisiondatabase/VOCtrainval_11-May-2009/VOCdevkit/"  # please replace with the location on your laptop where you unpacked the tarball
 image_size = 144    # image size that you will use for your network (input images will be resampled to this size), lower if you have troubles on your laptop (hint: use io.imshow to inspect the quality of the resampled images before feeding it into your network!)
-                    # neem iets dat deelbaar is door 8
-if True:
+                    # image_size should be divisible by 8 for the best result
+if False:
     # step1 - build list of filtered filenames
     annotation_folder = os.path.join(voc_root_folder, "VOC2009/Annotations/")
     annotation_files = os.listdir(annotation_folder)
@@ -73,15 +72,9 @@ if True:
     x_val, y_val = build_classification_dataset(val_files)
     print('%i validation images from %i classes' %(x_val.shape[0],  y_train.shape[1]))
     
-    # ------------------------
-    
-    # store output -> dat elke keer maken duurt te lang
-    
-# =============================================================================
-#     io.imshow(x_val[0])
-#     plt.show()
-#     sys.exit()
-# =============================================================================
+# ^^^ Given code ^^^
+# -----------------------------------------------------------------------------
+# ⌄⌄⌄ Own code ⌄⌄⌄
     
     # Saving the objects:
     with open('x_train.txt', 'wb') as f:
@@ -92,7 +85,6 @@ if True:
         pickle.dump(x_val, f)
     with open('y_val.txt', 'wb') as f:
         pickle.dump(y_val, f)
-        
 else:
     # Getting back the objects:
     with open('x_train.txt', 'rb') as f:
@@ -104,23 +96,17 @@ else:
     with open('y_val.txt', 'rb') as f:
         y_val = pickle.load(f)
 
-# from here, you can start building your model
-# you will only need x_train and x_val for the autoencoder
-# you should extend the above script for the segmentation task (you will need a slightly different function for building the label images)
 
-#handige links
-# https://www.youtube.com/watch?v=hbU7nbVDzGE PCA vs auto-encoders
-# https://blog.keras.io/building-autoencoders-in-keras.html voorbeeldcode
-# https://stackoverflow.com/questions/48243360/how-to-determine-the-filter-parameter-in-the-keras-conv2d-function filtersize
-# https://stackoverflow.com/questions/51877834/fine-tuning-of-keras-autoencoders-of-cat-images
-# https://github.com/shibuiwilliam/Keras_Autoencoder/blob/master/Cifar_Conv_AutoEncoder.ipynb
-# https://towardsdatascience.com/a-guide-to-an-efficient-way-to-build-neural-network-architectures-part-ii-hyper-parameter-42efca01e5d7
+# =============================================================================
+# 1. Constructing the auto-encoder
+#    - This auto-encoder model will take (image_size, image_size, 3) as input
+#      and will output an image of the same size.
+#    - The encoded layer has size (image_size/8,image_size/8,32) and will thus
+#      reduce the imput by a factor 6.
+# =============================================================================
+input_img = Input(shape=(image_size, image_size, 3))
 
-autoencoder = None
-'''
-input_img = Input(shape=(image_size, image_size, 3))  # adapt this if using `channels_first` image data format, origineel (28,28,1) dit wilt zeggen 28 breed 28 hoog en 1 diep (zwart wit dus)
-
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img) #eerste argument is the dimensionality of the output space (i.e. the number of output filters in the convolution).
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img) 
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
@@ -131,12 +117,8 @@ x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+
 encoded = MaxPooling2D((2, 2), padding='same')(x)
-
-# at this point the representation is (27, 27, 8) i.e. 128-dimensional
-
-# grotere filter (5 op 5 bijvoorbeeld)
-# groter aantal filter
 
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
@@ -155,14 +137,32 @@ autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
 
+# =============================================================================
+# 2. Training the model
+#    - When trainModel is TRUE, the model will learn how to encode pictures 
+#      by encoding and decoding the train images and try to learn a way that 
+#      minimizes the mean square error.
+#    - The weights are stored in the data folder.
+#    - The training metrics accuracy, loss, validation accuracy and validation
+#      accuracy are plotted when the training is done.
+#    - When loadModelFromDisk is TRUE, the model will load previously trained 
+#      weights and possibly train them further based on 
+#      the trainModel parameter.
+#    - When the weights are loaded in a picture is encoded and decoded to see
+#      the result.
+# =============================================================================
+trainModel = False
+loadModelFromDisk = True
 
-#netwerk van 0 trainen
-if False:
+if loadModelFromDisk:
+    autoencoder.load_weights("data/weights.h5")
+if trainModel:
     history = autoencoder.fit(x_train, x_train,
-                    epochs=1000,
+                    epochs=2,
                     batch_size=64,
                     shuffle=True,
                     validation_data=(x_val, x_val))
+    
     tijd = datetime.datetime.now()
     tijdstring = tijd.strftime("%H:%M:%S").replace(":", "_")
     autoencoder.save_weights("data/weights"+tijdstring+".h5")
@@ -184,105 +184,86 @@ if False:
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
+if ((not trainModel) and (not loadModelFromDisk)):
+    print('model is untrained, part runPart3_1 will be skipped')
+    runPart3_1 = False
 else:
-    #netwerk nog verder trainen
-    if False:
-        autoencoder.load_weights("data/weights.h5")
-        autoencoder.fit(x_train, x_train,
-                    epochs=100,
-                    batch_size=64,
-                    shuffle=True,
-                    validation_data=(x_val, x_val))
-        tijd = datetime.datetime.now()
-        tijdstring = tijd.strftime("%H:%M:%S").replace(":", "_")
-        autoencoder.save_weights("data/weights"+tijdstring+".h5")
-    else:
-        autoencoder.load_weights("data/weights.h5")
-
-
-
-
-#vergelijk input met output
-io.imshow(x_val[0])
-plt.show()
-testPicture = np.asarray([x_val[0].tolist()])
-uitkomst = autoencoder.predict(testPicture)[0]
-io.imshow(uitkomst)
-plt.show()
-'''
+    # show result of the model
+    io.imshow(x_val[0])
+    plt.show()
+    testPicture = np.asarray([x_val[0].tolist()])
+    uitkomst = autoencoder.predict(testPicture)[0]
+    io.imshow(uitkomst)
+    plt.show()
 
 # =============================================================================
-# CLASSIFICATIE (deel 3)
-# logic regression
-# https://towardsdatascience.com/understanding-logistic-regression-9b02c2aec102
-#  -> 'As discussed earlier, to deal with outliers, Logistic Regression uses Sigmoid function.'
-#
-# fine-tuning
-# https://www.learnopencv.com/keras-tutorial-fine-tuning-using-pre-trained-models/
-#
-# loss function
-# https://machinelearningmastery.com/how-to-choose-loss-functions-when-training-deep-learning-neural-networks/
-#  -> model.compile(loss='mean_squared_logarithmic_error', optimizer=opt, metrics=['mse'])
-# ik denk dit -> model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+# CLASSIFICATION
+# - Part3_1 is a model that takes the encoder from the previous auto-encoder 
+#   and sticks 2 fully connected layers that will preform the task of classifier
+#       - When runPart3_1 is True the model will be constructed and when
+#         trainPart3_1 is True it will train the model from scratch, when it is
+#         False, the weights are loaded in. NOTE: this model can only train
+#         weights of the new layers. The encoder layers still have their 
+#         weights from the previous section.
+# - Part3_2 is the same model as 3_1 but now all the weights are trainable
+# - In both cases, training metrics will be printed, newly traind weights are
+#   Stored to disk and the model is tested on 20 randomly selected validation
+#   images. Those results will be printed in the console.
 # =============================================================================
+runPart3_1 = True
+trainPart3_1 = False
+runPart3_2 = True
+trainPart3_2 = False
 
-#-------------------------
-#EERSTE MODEL
-# https://www.pyimagesearch.com/2018/05/07/multi-label-classification-with-keras/
-# https://stackoverflow.com/questions/42081257/keras-binary-crossentropy-vs-categorical-crossentropy-performance
-# https://www.depends-on-the-definition.com/guide-to-multi-label-classification-with-neural-networks/
-#-------------------------
-if False:
-    # https://medium.com/@vijayabhaskar96/multi-label-image-classification-tutorial-with-keras-imagedatagenerator-cd541f8eaf24
-    #autoencoder.load_weights("data/weights.h5")
-    #encoderlayers = autoencoder.layers[:8] # geen idee of dit werkt
-    
-    '''
-    # Freeze the layers
-    for i in range(1,8,2):
-        encoderlayers[i].trainable = False
-    '''
-    
-    input_dim = 10368 #18*18*64
-    output_dim = nb_classes = 5 
+input_dim = 10368 #18*18*32
+output_dim = nb_classes = len(filter)
+
+y_train_parts = []
+y_val_parts = []
+for i in range (0,nb_classes):
+    helper = y_train[:,i].ravel()
+    helper2 = helper.copy()
+    helper = np.append(helper,helper2,axis=0)
+    y_train_parts.append(helper)
+    y_val_parts.append(y_val[:,i].ravel())
+
+x_trainMirror = x_train[...,::-1,:].copy()
+x_train = np.append(x_train,x_trainMirror, axis=0)
+
+if runPart3_1:
     
     for i in range(len(autoencoder.layers)):
         autoencoder.layers[i].trainable = False
+        
     x = autoencoder.layers[12].output
 
     x = Flatten()(x)
     x = Dense(2048, input_dim = input_dim, activation='relu')(x)
     x = Dropout(0.5)(x)
-    x = Dense(2048, input_dim = 4096, activation='relu')(x)
+    x = Dense(2048, input_dim = 2048, activation='relu')(x)
     x = Dropout(0.5)(x)
     
-    output1 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output2 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output3 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output4 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output5 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
+    output1 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output2 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output3 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output4 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output5 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
 
     modelPredict = Model(autoencoder.inputs,[output1,output2,output3,output4,output5])    
     
     plot_model(modelPredict, to_file='model_plot_modelPredict.png', show_shapes=True, show_layer_names=True)
     modelPredict.summary()
     
-    modelPredict.compile(optimizer = "adam", loss = ["mse","mse","mse","mse","mse"], metrics=[])
-    #train van 0
-    if True:
-        y_train_parts = []
-        y_val_parts = []
-        for i in range (0,5):
-            y_train_parts.append(y_train[:,i].ravel())
-            y_val_parts.append(y_val[:,i].ravel())
-        
-        history = modelPredict.fit(x_train, y_train_parts,shuffle=True, batch_size=64, nb_epoch=50,verbose=1, validation_data=(x_val, y_val_parts))
+    modelPredict.compile(optimizer = "adam", loss = ["binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy"], metrics=[])
+    
+    
+    if trainPart3_1:
+        history = modelPredict.fit(x_train, y_train_parts,shuffle=True, batch_size=64, nb_epoch=30,verbose=1, validation_data=(x_val, y_val_parts))
         tijd = datetime.datetime.now()
         tijdstring = tijd.strftime("%H:%M:%S").replace(":", "_")
-        modelPredict.save_weights("data/weightsdeel31new"+tijdstring+".h5")
+        modelPredict.save_weights("data/weightsdeel31"+tijdstring+".h5")
         
         print(history.history.keys())
-        # summarize history for loss
         plt.plot(history.history['loss'])
         plt.plot(history.history['val_loss'])
         plt.title('model loss')
@@ -290,7 +271,6 @@ if False:
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
         plt.show()
-        
     else:
         modelPredict.load_weights("data/weightsdeel31.h5")
     
@@ -301,16 +281,16 @@ if False:
     predictedValuesBU = np.copy(predictedValues)
     
     # accuracy test
-    hoogsteClassificatie = np.apply_along_axis(np.argmax,1,predictedValues[0])
+    highestClassification = np.apply_along_axis(np.argmax,1,predictedValues[0])
     
-    juistVoorspeld = 0
+    predictedRight = 0
     for i,row in enumerate(y_val):
-        if row[hoogsteClassificatie[i]] == 1:
-            juistVoorspeld += 1
-    accuracy = juistVoorspeld/len(y_val)
+        if row[highestClassification[i]] == 1:
+            predictedRight += 1
+    accuracy = predictedRight/len(y_val)
     print("accuracy is: " + str(accuracy))
     
-    for _ in range (0,50):
+    for _ in range (0,20):
         i = random.randint(0, len(y_val))
         io.imshow(x_val[i])
     
@@ -325,25 +305,12 @@ if False:
         plt.show()
     
 #-------------------------
-#TWEEDE MODEL
+#SECOND MODEL
 #-------------------------
-if True:
-    # https://medium.com/@vijayabhaskar96/multi-label-image-classification-tutorial-with-keras-imagedatagenerator-cd541f8eaf24
-    #encoderlayers = autoencoder.layers[:8] # geen idee of dit werkt
+if runPart3_2:
     
-    '''
-    # Freeze the layers
-    for i in range(1,8,2):
-        encoderlayers[i].trainable = False
-    '''
-    
-    
-    input_dim = 10368 #8*8*64
-    output_dim = nb_classes = 5 
-    
-    input_img = Input(shape=(image_size, image_size, 3))  # adapt this if using `channels_first` image data format, origineel (28,28,1) dit wilt zeggen 28 breed 28 hoog en 1 diep (zwart wit dus)
-    #kernel_initializer='random_uniform'
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img) #eerste argument is the dimensionality of the output space (i.e. the number of output filters in the convolution).
+    input_img = Input(shape=(image_size, image_size, 3))
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img) 
     x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
     x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
@@ -362,61 +329,51 @@ if True:
     x = Dense(2048, input_dim = 2048, activation='relu')(x)
     x = Dropout(0.5)(x)
     
-    output1 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output2 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output3 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output4 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
-    output5 = Dense(1, input_dim = 1024, activation = 'sigmoid')(x)
+    output1 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output2 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output3 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output4 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
+    output5 = Dense(1, input_dim = 2048, activation = 'sigmoid')(x)
 
     modelPredict = Model(input_img,[output1,output2,output3,output4,output5])    
     modelPredict.summary()
     modelPredict.compile(optimizer = "adam", loss = ["binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy"], metrics=[])
-    
-    y_train_parts = []
-    y_val_parts = []
-    for i in range (0,5):
-        helper = y_train[:,i].ravel()
-        helper2 = helper.copy()
-        helper = np.append(helper,helper2,axis=0)
-        y_train_parts.append(helper)
-        y_val_parts.append(y_val[:,i].ravel())
-    
-    x_trainMirror = x_train[...,::-1,:].copy()
-    x_train = np.append(x_train,x_trainMirror, axis=0)
 
-    history = modelPredict.fit(x_train, y_train_parts, batch_size=64,shuffle=True, nb_epoch=10,verbose=1, validation_data=(x_val, y_val_parts))
-    
-    print(history.history.keys())
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    
-    tijd = datetime.datetime.now()
-    tijdstring = tijd.strftime("%H:%M:%S").replace(":", "_")
-    modelPredict.save_weights("data/weightsdeel31new"+tijdstring+".h5")
+
+    if trainPart3_2:
+        history = modelPredict.fit(x_train, y_train_parts, batch_size=64,shuffle=True, nb_epoch=6,verbose=1, validation_data=(x_val, y_val_parts))
+        
+        print(history.history.keys())
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+        
+        tijd = datetime.datetime.now()
+        tijdstring = tijd.strftime("%H:%M:%S").replace(":", "_")
+        modelPredict.save_weights("data/weightsdeel32"+tijdstring+".h5")
+    else:
+        modelPredict.load_weights("data/weightsdeel32.h5")
     
     predictedValues = modelPredict.predict(x_val)
-    
     predictedValues = np.asarray(predictedValues)
     predictedValues = np.transpose(predictedValues)
     predictedValuesBU = np.copy(predictedValues)
     
     # accuracy test
-    hoogsteClassificatie = np.apply_along_axis(np.argmax,1,predictedValues[0])
+    highestClassification = np.apply_along_axis(np.argmax,1,predictedValues[0])
     
-    juistVoorspeld = 0
+    predictedRight = 0
     for i,row in enumerate(y_val):
-        if row[hoogsteClassificatie[i]] == 1:
-            juistVoorspeld += 1
-    accuracy = juistVoorspeld/len(y_val)
+        if row[highestClassification[i]] == 1:
+            predictedRight += 1
+    accuracy = predictedRight/len(y_val)
     print("accuracy is: " + str(accuracy))
     
-    for _ in range (0,50):
+    for _ in range (0,20):
         i = random.randint(0, len(y_val))
         io.imshow(x_val[i])
     
